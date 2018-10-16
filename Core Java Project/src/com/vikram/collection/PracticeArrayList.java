0ef424ea89c856f51e35b63dc85e408f21416014
iijnm <iijnm@192.168.0.5>
2018-10-16 22:52:12 +0530

    my collection project

---
+++ Core Java Project/src/com/vikram/collection/PracticeArrayList.java
@@ -0,0 +1,13 @@
+package com.vikram.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+//Default size of arraylist is 10 and increase by with capacity 50% or old size*1.5
+public class PracticeArrayList {
+	
+	public static void main(String[] args) {
+		List<Integer> number = new ArrayList<>();
+		
+	}
+
+}
