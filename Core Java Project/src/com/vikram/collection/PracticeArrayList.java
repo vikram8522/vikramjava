2b8253c589131ae8e3a3d20c1763a53286970c5a
vikram8522 <44172820+vikram8522@users.noreply.github.com>
2020-04-22 20:34:06 +0530

    ee

--- Core Java Project/src/com/vikram/collection/PracticeArrayList.java
+++ Core Java Project/src/com/vikram/collection/PracticeArrayList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 //Default size of arraylist is 10 and increase by with capacity 50% or old size*1.5
 public class PracticeArrayList {
-	
+	//here also some issue
 	public static void main(String[] args) {
 		List<Integer> number = new ArrayList<>();
 		number.add(10);
