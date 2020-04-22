a31d4f0c7f8855139eb92a4e6cb4c7293045f976
Vikram <vikrarat@in.ibm.com>
2020-04-23 01:06:01 +0530

    eclipse2

--- Core Java Project/src/com/vikram/collection/PracticeArrayList.java
+++ Core Java Project/src/com/vikram/collection/PracticeArrayList.java
@@ -10,12 +10,7 @@ public class PracticeArrayList {
 		number.add(20);
 		number.add(30);
 		System.out.println(number.size());
-		//This is for loop1
-<<<<<<< Upstream, based on origin/master
-=======
-		//This is from eclipse
->>>>>>> 1616844 This second from eclipse after merge
-		//1 This is from github
+		//2
 		for(Integer num:number){
 			System.out.println(num);
 
