331a60f61e9b9fd253f686dd840025a98b643c97
vikram8522 <44172820+vikram8522@users.noreply.github.com>
2020-07-29 15:29:38 +0530

    Update PracticeArrayList.java
    added comment from github

--- Core Java Project/src/com/vikram/collection/PracticeArrayList.java
+++ Core Java Project/src/com/vikram/collection/PracticeArrayList.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class PracticeArrayList {
 	
 	public static void main(String[] args) {
-		//added comment from git hub
+		//added comment from git hub1
 		List<Integer> number = new ArrayList<>();
 		number.add(10);
 		number.add(20);
